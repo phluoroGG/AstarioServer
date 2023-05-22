401774ae4ca06699760a823682ffeadb040e62f3
phluoroGG <phluoro@gmail.com>
2023-05-22 16:24:26 +0300

    Create post entity

--- src/main/java/com/tp31/astarioserver/model/post/Post.java
+++ src/main/java/com/tp31/astarioserver/model/post/Post.java
@@ -1,2 +1,49 @@
-package com.tp31.astarioserver.model.post;public class post {
+package com.tp31.astarioserver.model.post;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String header;
+    private String text;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", header='" + header + '\'' +
+                ", text='" + text + '\'' +
+                '}';
+    }
 }
