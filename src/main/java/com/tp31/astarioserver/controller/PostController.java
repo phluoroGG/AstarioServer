133bd0c14362237081673e5be5feb5094d6fd27d
phluoroGG <phluoro@gmail.com>
2023-05-22 16:38:10 +0300

    Add Post Controller file to project

--- src/main/java/com/tp31/astarioserver/controller/PostController.java
+++ src/main/java/com/tp31/astarioserver/controller/PostController.java
@@ -1,2 +1,27 @@
-package com.tp31.astarioserver.controller;public class PostController {
+package com.tp31.astarioserver.controller;
+
+import com.tp31.astarioserver.model.post.Post;
+import com.tp31.astarioserver.model.post.PostDao;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostController {
+
+    @Autowired
+    private PostDao postDao;
+
+    @GetMapping("/post/get-all")
+    public List<Post> getAllEmployees() {
+        return postDao.getAllPosts();
+    }
+
+    @PostMapping("/post/save")
+    public Post save(@RequestBody Post post) {
+        return postDao.save(post);
+    }
 }
